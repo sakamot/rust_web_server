f608f75dd6c560444dec5d0dcb62e76a93f83e67
sakamot <matuhana.8@gmail.com>
2020-12-16 17:39:53 +0900

    add 404

--- src/main.rs
+++ src/main.rs
@@ -25,31 +25,31 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    // ↓↓↓
-    // Request: GET / HTTP/1.1
-    // Host: 127.0.0.1:7878
-    // Connection: keep-alive
-    // Cache-Control: max-age=0
-    // Upgrade-Insecure-Requests: 1
-    // User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36
-    // Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
-    // Sec-Fetch-Site: none
-    // Sec-Fetch-Mode: navigate
-    // Sec-Fetch-User: ?1
-    // Sec-Fetch-Dest: document
-    // Accept-Encoding: gzip, deflate, br
-    // Accept-Language: ja,en-US;q=0.9,en;q=0.8
+    // バイト文字列に変換
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let mut file = File::open("hello.html").unwrap();
+    if buffer.starts_with(get) {
+        let mut file = File::open("hello.html").unwrap();
 
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
-    // 文字列をバイトに変換
-    stream.write(response.as_bytes()).unwrap();
-    // flushは待機し、 バイトが全て接続に書き込まれるまでプログラムが継続するのを防ぐ
-    stream.flush().unwrap();
+        // 文字列をバイトに変換
+        stream.write(response.as_bytes()).unwrap();
+        // flushは待機し、 バイトが全て接続に書き込まれるまでプログラムが継続するのを防ぐ
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let mut file = File::open("404.html").unwrap();
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
